58cc47724b1961c18632a80ee99c219c702fc91a
bipin-01 <poudel7700@gmail.com>
2022-01-21 03:42:41 -0500

    learning nodeJs backend concept

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
-console.log('hi');
-var a = 1;
-console.log(a);
+const { fstat } = require('fs');
+const http = require('http');
+const dt = require("./dateTime");
+const fs = require("fs");
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    fs.readFile('./demofile1.html', function(err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        res.end();
+    });
+});
+
+server.listen(port, () => {
+    console.log(dt.myDateTime());
+    console.log(`server running at port ${port}`);
+});
